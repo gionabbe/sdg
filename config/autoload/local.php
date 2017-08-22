46f5f7c9dcfb1ef8cdb2d6b7c98020dce7a54d9b
GionHobby <jedipann@gmail.com>
2017-08-23 00:44:06 +0700

    Add files via upload

---
+++ config/autoload/local.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ *
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
+ */
+
+return [
+    'db' => [
+        'driver'    => 'PdoMysql',
+        'hostname'  => '127.0.0.1',
+        'database'  => 'sdg_db',
+        'user'      => 'root',
+        'password'  => 'toor',
+    ],
+    'service_manager' => [
+        'factories' => [
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ]
+    ]
+];
+
